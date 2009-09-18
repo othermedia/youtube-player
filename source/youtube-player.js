82ae79849aa03d115d19982db80559af3a1e111c
James Coglan <jcoglan@googlemail.com>
2009-09-18 19:57:13 +0100

    Initial commit: new Helium project.

---
+++ source/youtube-player.js
@@ -0,0 +1,14 @@
+/**
+ * class YoutubePlayer
+ **/
+YoutubePlayer = new JS.Class('YoutubePlayer', {
+    extend: {
+        VERSION: '1.0.0'
+    },
+    
+    /**
+     * new YoutubePlayer()
+     **/
+    initialize: function() {}
+});
+
